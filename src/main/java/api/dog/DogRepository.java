94e5303ebd9df5d12b0293265b48765ffca6d274
Baekgyu <baekgyu.dev@gmail.com>
2024-03-20 14:15:36 +0900

    mod: DogRepository

--- src/main/java/api/dog/DogRepository.java
+++ src/main/java/api/dog/DogRepository.java
@@ -1,12 +1,6 @@
 package api.dog;
 
-import api.user.User;
-import jakarta.persistence.EntityManager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface DogRepository extends JpaRepository<Dog, Integer> { //JpaRepository 확장하기?
-
+public interface DogRepository extends JpaRepository<Dog, Integer> {
 }
