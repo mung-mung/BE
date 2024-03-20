408bebaeb7bdb1d990aead104f8d9a758dcf4846
Baekgyu <baekgyu.dev@gmail.com>
2024-03-20 09:31:10 +0900

    mod: createDogTable 쿼리 수정

--- sql/dog/createDogTable.sql
+++ sql/dog/createDogTable.sql
@@ -1,14 +1,12 @@
 drop table if exists dogs CASCADE;
 CREATE TABLE dogs (
-                       ID_DOG SERIAL,
-                       DOG_NAME VARCHAR(20) NOT NULL,
+                       ID SERIAL,
+                       NAME VARCHAR(20) NOT NULL,
                        OWNER_ID INT NOT NULL,
-                       OWNER_EMAIL VARCHAR(255) NOT NULL,
                        WALKER_ID INT NOT NULL,
-                       WALKER_EMAIL VARCHAR(255),
                        CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                        UPDATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       FOREIGN KEY (OWNER_ID, OWNER_EMAIL) REFERENCES USERS(ID, EMAIL),
-                       FOREIGN KEY (WALKER_ID, WALKER_EMAIL) REFERENCES USERS(ID, EMAIL),
-                       PRIMARY KEY (ID_DOG)
+                       FOREIGN KEY (OWNER_ID) REFERENCES USERS(ID),
+                       FOREIGN KEY (WALKER_ID) REFERENCES USERS(ID),
+                       PRIMARY KEY (ID)
 );
