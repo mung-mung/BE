2d29aba3b4bc1d9e46d4cc4d3f27b6f0b8328a99
Baekgyu <baekgyu.dev@gmail.com>
2024-03-21 00:28:41 +0900

    mod: 쿼리문에서 on delete cascade 설정

--- sql/user/owner/createOwnerTable.sql
+++ sql/user/owner/createOwnerTable.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS owner CASCADE;
 CREATE TABLE owner (
                        ID INT PRIMARY KEY,
-                       FOREIGN KEY (ID) REFERENCES user_account(ID)
+                       FOREIGN KEY (ID) REFERENCES user_account(ID) ON DELETE CASCADE
 );
--- sql/user/walker/createWalkerTable.sql
+++ sql/user/walker/createWalkerTable.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS walker CASCADE;
 CREATE TABLE walker (
                        ID INT PRIMARY KEY,
-                       FOREIGN KEY (ID) REFERENCES user_account(ID)
+                       FOREIGN KEY (ID) REFERENCES user_account(ID) ON DELETE CASCADE
 );
