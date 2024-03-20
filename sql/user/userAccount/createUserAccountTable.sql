677c7c844f5a59383ebd15184ecbad992be2117c
Baekgyu <baekgyu.dev@gmail.com>
2024-03-20 23:38:52 +0900

    fix: birthday 타입 수정

--- sql/user/userAccount/createUserAccountTable.sql
+++ sql/user/userAccount/createUserAccountTable.sql
@@ -7,7 +7,7 @@ CREATE TABLE user_account (
                        AVATAR_URL VARCHAR(255) DEFAULT 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png',
                        CONTACT VARCHAR(255) NOT NULL,
                        GENDER VARCHAR(20) NOT NULL CHECK (GENDER IN ('MALE', 'FEMALE', 'PREFER_NOT_TO_DISCLOSE')),
-                       BIRTHDAY TIMESTAMP NOT NULL,
+                       BIRTHDAY DATE NOT NULL,
                        CREATED_AT TIMESTAMP,
                        UPDATED_AT TIMESTAMP,
                        PRIMARY KEY (ID)
