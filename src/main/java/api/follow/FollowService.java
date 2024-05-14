5503b56c3c3e43288d770ae50c32371c3d6b5783
min0219 <superharry77@gmail.com>
2024-05-14 18:24:03 +0900

    Changes

--- src/main/java/api/follow/FollowService.java
+++ src/main/java/api/follow/FollowService.java
@@ -1,19 +1,25 @@
 package api.follow;
 
+import api.dog.DogRepository;
 import api.follow.dto.FollowDto;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FollowService {
     private final FollowRepository followRepository;
+    private final DogRepository dogRepository;
 
     @Autowired
-    public FollowService(FollowRepository followRepository) {
+    public FollowService(FollowRepository followRepository, DogRepository dogRepository) {
         this.followRepository = followRepository;
+        this.dogRepository = dogRepository;
     }
 
     @Transactional
@@ -23,22 +29,45 @@ public class FollowService {
     }
 
     @Transactional(readOnly = true)
-    public List<Follow> findAll() {
-
+    public List<FollowDto> findAllFollows() {
+        List<Follow> allfollows = followRepository.findAll();
+        List<FollowDto> followDtos = new ArrayList<>();
+        for (Follow follow : allfollows) {
+            FollowDto followDto = new FollowDto(follow.getFollowerId(), follow.getFolloweeId());
+            followDtos.add(followDto);
+        }
+        return followDtos;
     }
 
     @Transactional(readOnly = true)
-    public Follow findByfollowerId(int followerid) {
-
+    public FollowDto findByfollowerId(Integer followerid) {
+        Optional<Follow> optionalFollow = followRepository.findById(followerid);
+        if (optionalFollow.isPresent()) {
+            Follow follow = optionalFollow.get();
+            FollowDto followDto = new FollowDto(follow.getFollowerId(), follow.getFolloweeId());
+            return followDto;
+        }else{
+            return null;
+        }
     }
 
     @Transactional(readOnly = true)
-    public Follow findByfolloweeId(int followeeid) {
-
+    public FollowDto findByfolloweeId(Integer followeeid) {
+        Optional<Follow> optionalFollow = followRepository.findById(followeeid);
+        if (optionalFollow.isPresent()) {
+            Follow follow = optionalFollow.get();
+            FollowDto followDto = new FollowDto(follow.getFollowerId(), follow.getFolloweeId());
+            return followDto;
+        }else{
+            return null;
+        }
     }
 
     @Transactional
-    public Follow deleteFollow(Integer followeeid) {
-
+    public void deleteFollowById(Integer id) {
+        if(!followRepository.existsById(id)) {
+            throw new EntityNotFoundException("Follow not found with ID: " + id);
+        }
+        dogRepository.deleteById(id);
     }
 }
