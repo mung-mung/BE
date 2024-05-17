eb8c6da68b013fcf36f3af3953e098e84d1339f0
whsung0330 <whsung0330@gmail.com>
2024-05-17 20:10:35 +0900

    add: OAuthAttributes

--- src/main/java/api/oauth/CustomOAuth2UserService.java
+++ src/main/java/api/oauth/CustomOAuth2UserService.java
@@ -1,9 +1,39 @@
 package api.oauth;
 
+import api.user.userAccount.UserAccount;
+import api.user.userAccount.UserAccountRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 
+import java.util.Collection;
+import java.util.Map;
+
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserAccountRepository userAccountRepository;
 
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        //OAuth2 정보 가져옴
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> service = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = service.loadUser(userRequest);
+
+        Map<String, Object> originAttributes = oAuth2User.getAttributes();
+
+        //OAuth 서비스 ID
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        OAuth2Attributes attributes = OAuthAttributes.of(registrationId, originAttributes);
+//        UserAccount userAccount = saveOrUpdate(attributes);
+        String email = userAccount.getEmail();
+        Collection<? extends GrantedAuthority> authorities = authentication.createAuthorities(email);
+
+        return new OAuth2CustomUser(registrationId, originAttributes, authorities, email);
+    }
 }
---
+++ src/main/java/api/oauth/OAuthAttributes.java
@@ -0,0 +1,60 @@
+package api.oauth;
+
+import api.user.userAccount.UserAccount;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Map;
+
+@Getter
+@ToString
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributesKey;
+    private String name;
+    private String email;
+    private String gender;
+    private String ageRange;
+    private String profileImageUrl;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributesKey, String name, String email, String gender, String ageRange, String profileImageUrl) {
+        this.attributes = attributes;
+        this.nameAttributesKey = nameAttributesKey;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.ageRange = ageRange;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public static OAuthAttributes of(String socialName, Map<String, Object> attributes) {
+        if ("google".equals(socialName)) {
+            return ofGoogle("id", attributes);
+        }
+//        } else if ("kakao".equals(socialName)) {
+//            return ofKakao("sub", attributes);
+//        } else if ("naver".equals(socialName)) {
+//            return ofNaver("id", attributes);
+//        }
+
+        return null;
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name(String.valueOf(attributes.get("name")))
+                .email(String.valueOf(attributes.get("email")))
+                .profileImageUrl(String.valueOf(attributes.get("picture")))
+                .attributes(attributes)
+                .nameAttributesKey(userNameAttributeName)
+                .build();
+    }
+
+    public UserAccount toEntity() {
+        return UserAccount()
+    }
+}
+
+
