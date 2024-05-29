59d5b883e272bb53543a059664995dccfc607a7d
Baekgyu <baekgyu.dev@gmail.com>
2024-05-29 10:37:34 +0900

    fix: following

--- src/main/java/api/follow/Follow.java
+++ src/main/java/api/follow/Follow.java
@@ -3,17 +3,20 @@ package api.follow;
 import api.user.userAccount.UserAccount;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
 
 @EqualsAndHashCode
 @ToString
+@NoArgsConstructor
 @Getter
-@Setter
+@Entity
+@Table(name="follow")
 public class Follow {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "FOLLOWER_ID", nullable = false)
     private UserAccount follower;
@@ -22,8 +25,26 @@ public class Follow {
     @JoinColumn(name = "FOLLOWEE_ID", nullable = false)
     private UserAccount followee;
 
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "UPDATED_AT", nullable = false)
+    private LocalDateTime updatedAt;
+
     public Follow(UserAccount follower, UserAccount followee){
         this.follower = follower;
         this.followee = followee;
+    }
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.updatedAt = now;
     }
 }
